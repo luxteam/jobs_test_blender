f78d0fcac5c9d9046cc099ca681f16d680fb31bf
Andrey Alekseevich <magister.demens@gmail.com>
2018-01-12 15:42:04 +0300

    Update submodule; added runFull.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,3 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
-python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice 2 TestsFilter full
-
-
+python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice 2 TestsFilter small
