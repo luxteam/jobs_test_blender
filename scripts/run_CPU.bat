ae1f7156d4014ee6ba00fcb0287c9e0629c62336
kravchenko andrey <epozine@inbox.ru>
2018-01-26 17:44:24 +0300

    Add cpu bat/sh

---
+++ scripts/run_CPU.bat
@@ -0,0 +1,3 @@
+set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+
+python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice 0 TestsFilter small ResPath "C:\TestResources\BlenderAssets\scenes"
