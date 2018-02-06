72a8bfe80ce0c5c4a457826f2b969a2a07026c05
Kravchenko Andrey <epozine@inbox.ru>
2018-02-06 14:54:11 +0300

    Delete pause

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,3 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
 python ..\jobs_launcher\executeTests.py --test_package IES_Test --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice gpu TestsFilter small ResPath "C:\TestResources\BlenderAssets\scenes"
-
-pause
