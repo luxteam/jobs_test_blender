eacc4c70a9f4a8785cc1a048d8be7f1801622b51
Kravchenko Andrey <epozine@inbox.ru>
2018-02-08 11:06:36 +0300

    Refactoring

--- jobs/Scripts/Templates/template_smoke.py
+++ jobs/Scripts/Templates/template_smoke.py
@@ -1,83 +1,17 @@
-import bpy
-from rprblender import node_editor
-import addon_utils
-import datetime
-import sys
-import json
-import os
 
-def make_presets(test_name, passes):
+def main(test_name, passes):
 
-	#get scene name
 	Scenename = bpy.context.scene.name
-
 	bpy.data.scenes[Scenename].rpr.render.rendering_limits.iterations = passes
-
-	# Render device in RPR
-	
-	if '{render_mode}' == 'dual':
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = True
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = 'gpu'
-	else:
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = '{render_mode}'
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = False
-	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.gpu_count = 2
-	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.samples = 1
-	bpy.context.user_preferences.addons["rprblender"].preferences.settings.include_uncertified_devices = True
-
-	# frame range
-	bpy.data.scenes[Scenename].frame_start = 1
-	bpy.data.scenes[Scenename].frame_end = 1
-
-	# image format
 	bpy.data.scenes[Scenename].render.image_settings.file_format = 'JPEG'
-	bpy.data.scenes[Scenename].render.image_settings.quality = 80
-	bpy.data.scenes[Scenename].render.image_settings.color_mode = 'RGB'
 
-	# output
-	name_scene = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + test_name
-	output = r"{work_dir}" + "/Color/" + name_scene + "_##"
-	bpy.data.scenes[Scenename].render.filepath = output 
-	bpy.data.scenes[Scenename].render.use_placeholder = True
-	bpy.data.scenes[Scenename].render.use_file_extension = True
-	bpy.data.scenes[Scenename].render.use_overwrite = True
-
-	# start render animation
-	TIMER = datetime.datetime.now()
-	bpy.ops.render.render(animation=True,scene=Scenename)
-	Render_time = datetime.datetime.now() - TIMER
-
-	# get version of rpr addon
-	for mod_name in bpy.context.user_preferences.addons.keys():
-		if (mod_name == 'rprblender') : 
-			mod = sys.modules[mod_name]
-			ver = mod.bl_info.get('version')
-			version = str(ver[0]) + "." + str(ver[1]) + "." + str(ver[2])
-		 
-	# LOG
-	name_scene_for_json = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + test_name + "BL"
-	log_name = os.path.join(r'{work_dir}', name_scene_for_json + ".json")
-	report = {{}}
-	report['render_version'] = version
-	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
-	report['tool'] = "Blender " + bpy.app.version_string
-	report['file_name'] = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + test_name + "_01.jpg"
-	report['scene_name'] = bpy.path.basename(bpy.context.blend_data.filepath)
-	report['render_time'] = Render_time.total_seconds()
-	report['render_color_path'] = r"{work_dir}" + "/Color/" + name_scene + "_01.jpg"
-	report['date_time'] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
-	report['difference_color'] = "not compared yet"
-
-
-	with open(log_name, 'w') as file:
-		json.dump([report], file, indent=' ')
+	render(test_name, passes)
 
 def test_IES():
 
 	bpy.data.lamps["Lamp"].rpr_lamp.ies_file_name = r"C:\\TestResources\\BlenderAssets\\scenes\\Candle.fbm\\PD6R12ED010- PDM6835-694SNB.ies"
 
-def base_light():
+def test_base_light():
 
 	bpy.context.scene.world.rpr_data.environment.enable = False
 
@@ -103,16 +37,8 @@ def import_test():
 	bpy.context.scene.render.engine = 'RPR'
 	bpy.context.scene.world.rpr_data.environment.enable = True
 
-	#bpy.context.scene.objects['Cube'].hide_render = True
-	#bpy.context.scene.objects['Cube'].hide = True
-
-	# deselect all
 	bpy.ops.object.select_all(action='DESELECT')
-
-	# selection
 	bpy.data.objects['Cube'].select = True
-
-	# remove it
 	bpy.ops.object.delete() 
 
 	file_loc = r"C:\\TestResources\\BlenderAssets\\scenes\\example.obj"
@@ -134,8 +60,6 @@ def create_Uber2():
 	material = bpy.data.materials.new('Shader_mat')
 	mesh = bpy.context.object.data
 	
-
-	# create material nodetree and retrieve it
 	override = bpy.context.copy()
 	override['material'] = material
 	bpy.ops.rpr.op_material_add_nodetree(override)
@@ -153,18 +77,24 @@ def create_Uber2():
 
 
 if __name__ == '__main__':
-	import_test()
-	make_presets("Import", 30)
-	create_Uber2()
-	make_presets("Uber2", 30)
-	base_light()
-	make_presets("Base_Light", 30)
-	test_IES()
-	make_presets("IES Light", 30)
-	test_IBL_on()
-	make_presets("IBL_on", 30)
-	test_Sun()
-	make_presets("Sun", 30)
 
-	make_presets("Full_100", 100)
-	make_presets("Full_500", 500)
+	import_test()
+	main("Import_Test", 30)
+
+	create_Uber2()
+	main("Uber2_Test", 30)
+
+	test_base_light()
+	main("Base_Light_Test", 30)
+
+	test_IES()
+	main("IES_Light_Test", 30)
+
+	test_IBL_on()
+	main("IBL_Test", 30)
+
+	test_Sun()
+	main("Sun_Sky_Test", 30)
+
+	main("Full_Test", 100)
+	main("Full_Test", 500)
--- jobs/Scripts/Templates/template_sun_sky.py
+++ jobs/Scripts/Templates/template_sun_sky.py
@@ -1,20 +1,10 @@
-import bpy
-import addon_utils
-import datetime
-import sys
-import json
-import os
 
 def main(type_sun_sky, quality, param1, param2):
 
-	#get scene name
 	Scenename = bpy.context.scene.name
 
-	# RPR Settings
-	if((addon_utils.check("rprblender"))[0] == False) : 
-	    addon_utils.enable("rprblender", default_set=True, persistent=False, handle_error=None)
-	bpy.data.scenes[Scenename].render.engine = "RPR"
 	bpy.data.scenes[Scenename].rpr.render.rendering_limits.iterations = {pass_limit}
+	bpy.data.scenes[Scenename].render.image_settings.file_format = 'JPEG'
 
 
 	bpy.context.scene.world.rpr_data.environment.type = 'SUN_SKY'
@@ -33,91 +23,27 @@ def main(type_sun_sky, quality, param1, param2):
 		bpy.context.scene.world.rpr_data.environment.sun_sky.time_hours = param1
 		bpy.context.scene.world.rpr_data.environment.sun_sky.time_minutes = param2
 
-	# Render device in RPR
-	if '{render_mode}' == 'dual':
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = True
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = 'gpu'
-	else:
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = '{render_mode}'
-		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = False
-	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.gpu_count = 2
-	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.samples = 1
-	bpy.context.user_preferences.addons["rprblender"].preferences.settings.include_uncertified_devices = True
+	render(type_sun_sky, quality, param1, param2)
 
-	# frame range
-	bpy.data.scenes[Scenename].frame_start = 1
-	bpy.data.scenes[Scenename].frame_end = 1
-
-	# image format
-	bpy.data.scenes[Scenename].render.image_settings.file_format = 'JPEG'
-	bpy.data.scenes[Scenename].render.image_settings.quality = 80
-	bpy.data.scenes[Scenename].render.image_settings.color_mode = 'RGB'
-
-	# output
-	name_scene = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + type_sun_sky + "_" + quality + "_" + str(param1) + "_" + str(param2)
-	output = r"{work_dir}" + "/Color/" + name_scene + "_##"
-	bpy.data.scenes[Scenename].render.filepath = output 
-	bpy.data.scenes[Scenename].render.use_placeholder = True
-	bpy.data.scenes[Scenename].render.use_file_extension = True
-	bpy.data.scenes[Scenename].render.use_overwrite = True
-
-	# start render animation
-	TIMER = datetime.datetime.now()
-	bpy.ops.render.render(animation=True,scene=Scenename)
-	Render_time = datetime.datetime.now() - TIMER
-
-	# get version of rpr addon
-	for mod_name in bpy.context.user_preferences.addons.keys():
-	    if (mod_name == 'rprblender') : 
-	        mod = sys.modules[mod_name]
-	        ver = mod.bl_info.get('version')
-	        version = str(ver[0]) + "." + str(ver[1]) + "." + str(ver[2])
-	     
-	# LOG
-	name_scene_for_json = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + type_sun_sky + "_" + quality + "_" + str(param1) + "_" + str(param2) + "BL"
-	log_name = os.path.join(r'{work_dir}', name_scene_for_json + ".json")
-	report = {{}}
-	report['render_version'] = version
-	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
-	report['tool'] = "Blender " + bpy.app.version_string
-	report['file_name'] = bpy.path.basename(bpy.context.blend_data.filepath) + "_" + type_sun_sky + "_" + quality + "_" + str(param1) + "_" + str(param2) + "_01.jpg"
-	report['scene_name'] = bpy.context.scene.name
-	report['render_time'] = Render_time.total_seconds()
-	report['render_color_path'] = r"{work_dir}" + "/Color/" + name_scene + "_01.jpg"
-	report['date_time'] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
-	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
-	report['difference_color'] = "not compared yet"
-
-
-	with open(log_name, 'w') as file:
-		json.dump([report], file, indent=' ')
 
 if __name__ == "__main__":
-	main('analytical_sky', 'small', 0, 0.523599)
-	main('analytical_sky', 'normal', 0, 0.523599)
-	main('analytical_sky', 'high', 0, 0.523599)
-	main('date_time_location', 'small', 12, 0)	
-	main('date_time_location', 'normal', 12, 0)	
-	main('date_time_location', 'high', 12, 0)
-	main('date_time_location', 'normal', 0, 0)	
-	main('date_time_location', 'normal', 6, 0)	
-	main('date_time_location', 'normal', 18, 0)	
-	main('date_time_location', 'normal', 23, 59)	
-	main('analytical_sky', 'normal', 0, 0)
-	main('analytical_sky', 'normal', 0, 0.785398)
-	main('analytical_sky', 'normal', 0, 1.5708)
-	main('analytical_sky', 'normal', 1.5708, 0)
-	main('analytical_sky', 'normal', 1.5708, 0.785398)
-	main('analytical_sky', 'normal', 1.5708, 1.5708)
-	main('analytical_sky', 'normal', 3.14159, 0)
-	main('analytical_sky', 'normal', 3.14159, 0.785398)
-	main('analytical_sky', 'normal', 3.14159, 1.5708)
-	main('analytical_sky', 'normal', 4.71239, 0)
-	main('analytical_sky', 'normal', 4.71239, 0.785398)
-	main('analytical_sky', 'normal', 4.71239, 1.5708)
-	main('analytical_sky', 'normal', 6.28319, 0)
-	main('analytical_sky', 'normal', 6.28319, 0.785398)
-	main('analytical_sky', 'normal', 6.28319, 1.5708)
+
+	type_sun_sky = ['analytical_sky', 'date_time_location']
+	qualities = ['small', 'normal', 'high']
+	times = [0, 6, 12, 24]
+	azimuths = [0, 1.5708, 3.14159, 4.71239, 6.28319]
+	altitudes = [0, 0.785398, 1.5708] 
+
+	for quality in qualities:
+		main(type_sun_sky[0], quality, 0, 0.523599)
+		main(type_sun_sky[1], quality, 12, 0)
+
+	for time in times:
+		main(type_sun_sky[1], qualities[1], time, 0)
+
+	analytical_combinations = [ (azimuth, altitude) for azimuth in azimuths for altitude in altitudes]
+	for each in test_combinations:
+		main(type_sun_sky[0], quality[1], each[0], each[1])
 
 
 
