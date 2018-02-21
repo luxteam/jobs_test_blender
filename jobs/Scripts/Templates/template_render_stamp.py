78594810fa2db61ed70986b23c3312c4af263205
Kravchenko Andrey <epozine@inbox.ru>
2018-02-21 15:37:30 +0300

    Fix stamp test

--- jobs/Scripts/Templates/template_render_stamp.py
+++ jobs/Scripts/Templates/template_render_stamp.py
@@ -10,12 +10,12 @@ def main(stamp, stamp_value):
 	bpy.data.scenes[Scenename].rpr.use_render_stamp = True
 	bpy.data.scenes[Scenename].rpr.render_stamp = stamp_value
 
-	render(stamp_value)
+	render(stamp)
 
 if __name__ == "__main__":
 	
-	main("default_stamp", "Radeon ProRender for Blender %b | %h | Time: %pt | Passes: %pp | Objects: %so | Lights: %sl", "gpu")
-	main("gpu_stamp", "Radeon ProRender for Blender %b | CPU %c | GPU %g | Render mode %r | Render device %h", "gpu")
-	main("additional_info", "Radeon ProRender for Blender %b | Computer name %i | Current date %d", "gpu")
+	main("default_stamp", "Radeon ProRender for Blender %b | %h | Time: %pt | Passes: %pp | Objects: %so | Lights: %sl")
+	main("gpu_stamp", "Radeon ProRender for Blender %b | CPU %c | GPU %g | Render mode %r | Render device %h")
+	main("additional_info", "Radeon ProRender for Blender %b | Computer name %i | Current date %d")
 
 
