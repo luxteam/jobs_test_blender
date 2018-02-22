320d147f1d9b2de8640ec6dcf7c5df800788f0f6
Kravchenko Andrey <epozine@inbox.ru>
2018-02-22 16:12:20 +0300

    Update IBL test. Update smoke test.

--- jobs/Scripts/Templates/template_IBL.py
+++ jobs/Scripts/Templates/template_IBL.py
@@ -12,14 +12,20 @@ def main(test_combination):
 	bpy.context.scene.world.rpr_data.environment.ibl.intensity = test_combination[1]
 	if (test_combination[0] == 'IBL'):
 		bpy.context.scene.world.rpr_data.environment.ibl.use_ibl_map = True
-		bpy.context.scene.world.rpr_data.environment.ibl.ibl_image = bpy.data.images.load(r"C:\TestResources\BlenderAssets\scenes\ibl_test.exr", True)
+		bpy.context.scene.world.rpr_data.environment.ibl.ibl_image = bpy.data.images.load(r"C:\TestResources\BlenderAssets\scenes\Tropical_Beach_3k.hdr", True)
+		render("Tropical_Beach_hdr", test_combination[1])
 
-	render(test_combination[0], test_combination[1])
+		bpy.context.scene.world.rpr_data.environment.ibl.use_ibl_map = True
+		bpy.context.scene.world.rpr_data.environment.ibl.ibl_image = bpy.data.images.load(r"C:\TestResources\BlenderAssets\scenes\ibl_test.exr", True)
+		render("city_exr", test_combination[1])
+	else:
+		render(test_combination[0], test_combination[1])
 
 if __name__ == "__main__":
 
 	values = [0, 1, 2, 3, 5, 7, 10]
 	IBL_types = ['COLOR', 'IBL']
+	IBL_mapes = ['IBL_tropical_beach_hdr', 'IBL_city_exr']
 
 	test_combinations = [(ibl_type, value) for ibl_type in IBL_types for value in values]
 	for each in test_combinations:
