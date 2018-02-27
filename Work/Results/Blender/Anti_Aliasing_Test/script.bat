f58f278b484b22ed561e53f5a9ee41247fb1abe1
Kravchenko Andrey <epozine@inbox.ru>
2018-02-27 10:40:49 +0300

    run bat

---
+++ Work/Results/Blender/Anti_Aliasing_Test/script.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Blender Foundation\Blender\blender.exe" -b "C:\TestResources\BlenderAssets\scenes\ComplexTestUber.blend" -P "C:\jobs_test_blender\Work\Results\Blender\Anti_Aliasing_Test\script.py"
---
+++ Work/Results/Blender/Anti_Aliasing_Test/script.py
@@ -0,0 +1,105 @@
+import bpy
+import addon_utils
+import datetime
+import sys
+import json
+import os
+from rprblender import node_editor
+
+def render(*argv):
+
+	#get scene name
+	Scenename = bpy.context.scene.name
+
+	# RPR Settings
+	if((addon_utils.check("rprblender"))[0] == False) : 
+	    addon_utils.enable("rprblender", default_set=True, persistent=False, handle_error=None)
+	bpy.data.scenes[Scenename].render.engine = "RPR"
+
+	# Render device in RPR
+	if 'gpu' == 'dual':
+		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = True
+		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = 'gpu'
+	else:
+		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type = 'gpu'
+		bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type_plus_cpu = False
+	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.gpu_count = 2
+	#bpy.context.user_preferences.addons["rprblender"].preferences.settings.samples = 1
+	bpy.context.user_preferences.addons["rprblender"].preferences.settings.include_uncertified_devices = True
+
+	# frame range
+	bpy.data.scenes[Scenename].frame_start = 1
+	bpy.data.scenes[Scenename].frame_end = 1
+
+	# image format
+	bpy.data.scenes[Scenename].render.image_settings.quality = 100
+	bpy.data.scenes[Scenename].render.image_settings.color_mode = 'RGB'
+
+	info = ""
+	for arg in argv:
+		info += "_"
+		info += str(arg).lower()
+
+	# output
+	name_scene = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + info
+	output = r"C:\jobs_test_blender\Work\Results\Blender\Anti_Aliasing_Test" + "/Color/" + name_scene + "_##"
+	bpy.data.scenes[Scenename].render.filepath = output 
+	bpy.data.scenes[Scenename].render.use_placeholder = True
+	bpy.data.scenes[Scenename].render.use_file_extension = True
+	bpy.data.scenes[Scenename].render.use_overwrite = True
+
+	# start render animation
+	TIMER = datetime.datetime.now()
+	bpy.ops.render.render(animation=True,scene=Scenename)
+	Render_time = datetime.datetime.now() - TIMER
+
+	# get version of rpr addon
+	for mod_name in bpy.context.user_preferences.addons.keys():
+	    if (mod_name == 'rprblender') : 
+	        mod = sys.modules[mod_name]
+	        ver = mod.bl_info.get('version')
+	        version = str(ver[0]) + "." + str(ver[1]) + "." + str(ver[2])
+	     
+	# LOG
+	name_scene_for_json = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + info + "_BL"
+	log_name = os.path.join(r'C:\jobs_test_blender\Work\Results\Blender\Anti_Aliasing_Test', name_scene_for_json + ".json")
+	report = {}
+	report['render_version'] = version
+	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
+	report['tool'] = "Blender " + bpy.app.version_string
+	report['file_name'] = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0] + info + "_01.jpg"
+	report['scene_name'] = bpy.path.basename(bpy.context.blend_data.filepath).split('.')[0]
+	report['render_time'] = Render_time.total_seconds()
+	report['render_color_path'] = r"Color/" + name_scene + "_01.jpg"
+	report['date_time'] = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+	report['render_device'] = bpy.context.user_preferences.addons["rprblender"].preferences.settings.device_type
+	report['difference_color'] = "not compared yet"
+
+
+	with open(log_name, 'w') as file:
+		json.dump([report], file, indent=' ')
+
+
+def main(test_combination):
+
+	#get scene name
+	Scenename = bpy.context.scene.name
+
+	bpy.data.scenes[Scenename].rpr.render.rendering_limits.iterations = 100
+	bpy.data.scenes[Scenename].render.image_settings.file_format = 'JPEG'
+
+	bpy.data.scenes[Scenename].rpr.render.aa.filter = test_combination[0]
+	bpy.data.scenes[Scenename].rpr.render.aa.radius = test_combination[1]
+
+	render(test_combination[0], test_combination[1])
+
+if __name__ == "__main__":
+
+	values = [0, 1.5, 5, 10]
+	filters = ['MITCHELL', 'LANCZOS','TRIANGLE', 'BOX', 'GAUSSIAN', 'BLACKMANHARRIS']
+
+	test_combinations = [ (aa_filter, value) for aa_filter in filters for value in values]
+	for each in test_combinations:
+		main(each)
+
+
