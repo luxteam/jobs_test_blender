4e7aee0c97db0179226916ba57248f82943359bc
Kravchenko Andrey <epozine@inbox.ru>
2018-02-27 10:36:10 +0300

    run bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,4 +1,4 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
-python ..\jobs_launcher\executeTests.py --test_package Render_Stamp_Test --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice gpu TestsFilter small ResPath "C:\TestResources\BlenderAssets\scenes"
+python ..\jobs_launcher\executeTests.py --test_package --tests_root ..\jobs --work_root ..\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice gpu TestsFilter full ResPath "C:\TestResources\BlenderAssets\scenes"
 pause
