c652ef4c98bdd074e1d9e3b73baf0a6b67671b10
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-04-06 21:30:42 +0300

    Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,8 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+set RENDER_DEVICE=%1
+set TESTS_FILTER=%2
+set TEST_PACKAGE=%3
+if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=2
+if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
 
 python ..\jobs_launcher\executeTests.py --test_package %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Blender --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\BlenderAssets\scenes"
