1fef383dbe01606c7d683abc0e33ee8fc6df405d
Andrey Kravchenko <epozine@inbox.ru>
2018-06-07 16:07:53 +0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,4 +15,4 @@ if [ "$TEST_PACKAGE" == "" ] || [ "$TEST_PACKAGE" == "null" ] ; then
     TEST_PACKAGE=smoke
 fi
 
-python ../jobs_launcher/executeTests.py --file_filter "$TEST_PACKAGE" --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice "$RENDER_DEVICE" TestsFilter "$TESTS_FILTER" ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 10 rx 0 ry 0
+python ../jobs_launcher/executeTests.py --file_filter "$TEST_PACKAGE" --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice "$RENDER_DEVICE" TestsFilter "$TESTS_FILTER" ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 100 rx 0 ry 0
