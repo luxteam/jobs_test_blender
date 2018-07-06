787a9aea97e02da81b5de0bed89055bde77c2ec5
Andrey Alekseevich <magister.demens@gmail.com>
2018-07-06 15:45:32 +0300

    fixed sh for multi-filters

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-RENDER_DEVICE="$1"
-TEST_PACKAGE="$2"
-TESTS_FILTER="$3"
+RENDER_DEVICE=$1
+FILE_FILTER=$2
+TESTS_FILTER=$3
 
 if [ "$RENDER_DEVICE" == "" ] || [ "$RENDER_DEVICE" == "null" ] ; then
     RENDER_DEVICE=gpu
 fi
 
-if [ "$TEST_PACKAGE" == "" ] || [ "$TEST_PACKAGE" == "null" ] ; then
-    TEST_PACKAGE=smoke
+if [ "$FILE_FILTER" == "" ] || [ "$FILE_FILTER" == "null" ] ; then
+    FILE_FILTER=smoke
 fi
 
-python ../jobs_launcher/executeTests.py --file_filter "$TEST_PACKAGE" --tests_filter "$TESTS_FILTER" --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice "$RENDER_DEVICE" ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 15 rx 0 ry 0
+python ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 5 rx 0 ry 0
