dc46a0b804d6ffa36ec03ee40b076483d9d9e455
Andrey Alekseevich <magister.demens@gmail.com>
2018-08-06 21:16:55 +0300

    move split_execution to cmd arguments

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,12 +3,4 @@ RENDER_DEVICE=$1
 FILE_FILTER=$2
 TESTS_FILTER="$3"
 
-if [ "$RENDER_DEVICE" == "" ] || [ "$RENDER_DEVICE" == "null" ] ; then
-    RENDER_DEVICE=gpu
-fi
-
-#if [ "$FILE_FILTER" == "" ] || [ "$FILE_FILTER" == "null" ] ; then
-#    FILE_FILTER=smoke
-#fi
-
-python ../jobs_launcher/executeTests.py --split_execution $4 --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 50 rx 0 ry 0
+python ../jobs_launcher/executeTests.py $4 $5 --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 50 rx 0 ry 0
