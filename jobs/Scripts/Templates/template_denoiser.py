5b99cfdc62919409a86868aa186d2b5bd92f5fc4
Nikita Larionov <larionov.nu@gmail.com>
2018-10-16 14:27:00 +0300

    fix denoiser passes

--- jobs/Scripts/Templates/template_denoiser.py
+++ jobs/Scripts/Templates/template_denoiser.py
@@ -3,7 +3,7 @@ def prerender(test_list):
 	Scenename = bpy.context.scene.name
 
 	bpy.context.scene.rpr.use_render_stamp = False
-	bpy.data.scenes[Scenename].rpr.render.rendering_limits.iterations = {pass_limit}
+	bpy.data.scenes[Scenename].rpr.render.rendering_limits.iterations = 100
 	bpy.data.scenes[Scenename].render.image_settings.file_format = 'JPEG'
 
 	if ({resolution_x} and {resolution_y}):
