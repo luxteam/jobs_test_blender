4e081a4b5d919d62d44e7be4a194255edbe97b0e
Andrey Kravchenko <epozine@inbox.ru>
2018-12-05 12:51:19 +0300

    fix encoding

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -60,11 +60,11 @@ def main(args):
     p = subprocess.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
 
-    with open(os.path.join(args.output, "renderTool.log"), 'w') as file:
+    with open(os.path.join(args.output, "renderTool.log"), 'w', encoding='utf-8') as file:
         stdout = stdout.decode("utf-8")
         file.write(stdout)
 
-    with open(os.path.join(args.output, "renderTool.log"), 'a') as file:
+    with open(os.path.join(args.output, "renderTool.log"), 'a', encoding='utf-8') as file:
         file.write("\n ----STEDERR---- \n")
         stderr = stderr.decode("utf-8")
         file.write(stderr)
