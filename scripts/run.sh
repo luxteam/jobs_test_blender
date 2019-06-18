2f6209ad52dd38b3491041070581adeb660e0aee
Andrey Alekseevich <magister.demens@gmail.com>
2019-06-18 12:21:16 +0300

    update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,4 @@ RENDER_DEVICE=$1
 FILE_FILTER=$2
 TESTS_FILTER="$3"
 
-python ../jobs_launcher/executeTests.py $4 $5 --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/BlenderAssets/scenes" PassLimit 50 rx 0 ry 0
+python ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/Blender2.8Assets" PassLimit 10 rx 0 ry 0
