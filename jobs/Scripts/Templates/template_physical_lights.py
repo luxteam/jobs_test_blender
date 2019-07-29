2dfcde0e2c4dfaef4d2809a6fc7d9c051aca5713
Andrei Kravchenko <epozine@inbox.ru>
2019-07-29 10:53:53 +0300

    Update Physical lights group

--- jobs/Scripts/Templates/template_physical_lights.py
+++ jobs/Scripts/Templates/template_physical_lights.py
@@ -27,7 +27,9 @@ def point_light(test_case, script_info, intensity, use_temperature, temperature,
 	set_value(lamp_data.rpr, 'temperature', temperature)
 	
 	if ies:
-		set_value(lamp_data.rpr, 'ies_file_name', os.path.join(r"{resource_path}", "ies", ies))
+		bpy.ops.image.open(filepath="//ies//{{}}".format(ies), directory="{resource_path}//ies//", files=[{{"name":"{{}}".format(ies)}}], \
+																														relative_path=True, show_multiview=False)
+		set_value(lamp_data.rpr, 'ies_file', bpy.data.images[ies])
 
 	render(test_case, script_info)
 	return 1
@@ -86,33 +88,33 @@ if __name__ == "__main__":
 
 	list_tests = [
 		# POINT
-		#["BL28_L_PL_001", ["Type: Point", "Intensity: 1000"], "Physical_Lights.blend", 'POINT', 1000, False, 6500, None], 
-		#["BL28_L_PL_002", ["Type: Point", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'POINT', 0, False, 6500, None], 
-		#["BL28_L_PL_003", ["Type: Point", "Intensity: 5000"], "Physical_Lights.blend", 'POINT', 5000, False, 6500, None],
-		#["BL28_L_PL_004", ["Type: Point", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'POINT', 1000, True, 6500, None],   
-		#["BL28_L_PL_005", ["Type: Point", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'POINT', 1000, True, 0, None], 
-		#["BL28_L_PL_006", ["Type: Point", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'POINT', 1000, True, 40000, None], 
-		#["BL28_L_PL_007", ["Type: Point", "Intensity: 1000", "IES file: 2.ies"], "Physical_Lights.blend", 'POINT', 1000, False, 6500, "2.ies"], 
+		["BL28_L_PL_001", ["Type: Point", "Intensity: 1000"], "Physical_Lights.blend", 'POINT', 1000, False, 6500, None], 
+		["BL28_L_PL_002", ["Type: Point", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'POINT', 0, False, 6500, None], 
+		["BL28_L_PL_003", ["Type: Point", "Intensity: 5000"], "Physical_Lights.blend", 'POINT', 5000, False, 6500, None],
+		["BL28_L_PL_004", ["Type: Point", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'POINT', 1000, True, 6500, None],   
+		["BL28_L_PL_005", ["Type: Point", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'POINT', 1000, True, 0, None], 
+		["BL28_L_PL_006", ["Type: Point", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'POINT', 1000, True, 40000, None], 
+		["BL28_L_PL_007", ["Type: Point", "Intensity: 1000", "IES file: 2.ies"], "Physical_Lights.blend", 'POINT', 1000, False, 6500, "2.ies"], 
 		# SUN
-		#["BL28_L_PL_008", ["Type: Sun", "Intensity: 50"], "Physical_Lights.blend", 'SUN', 50, False, 6500], 
-		#["BL28_L_PL_009", ["Type: Sun", "Intensity: 1000"], "Physical_Lights.blend", 'SUN', 1000, False, 6500], 
-		#["BL28_L_PL_010", ["Type: Sun", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'SUN', 0, False, 6500],
-		#["BL28_L_PL_011", ["Type: Sun", "Intensity: 5000"], "Physical_Lights.blend", 'SUN', 5000, False, 6500],  
-		#["BL28_L_PL_012", ["Type: Sun", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'SUN', 1000, True, 6500], 
-		#["BL28_L_PL_013", ["Type: Sun", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'SUN', 1000, True, 0], 
-		#["BL28_L_PL_014", ["Type: Sun", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'SUN', 1000, True, 40000], 
-		## SPOT
-		#["BL28_L_PL_015", ["Type: Spot", "Intensity: 2000"], "Physical_Lights.blend", 'SPOT', 2000, False, 6500, 1.309, 0.15], 
-		#["BL28_L_PL_016", ["Type: Spot", "Intensity: 1000"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 0.15],
-		#["BL28_L_PL_017", ["Type: Spot", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'SPOT', 0, False, 6500, 1.309, 0.15],
-		#["BL28_L_PL_018", ["Type: Spot", "Intensity: 5000"], "Physical_Lights.blend", 'SPOT', 5000, False, 6500, 1.309, 0.15],
-		#["BL28_L_PL_019", ["Type: Spot", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'SPOT', 1000, True, 6500, 1.309, 0.15],
-		#["BL28_L_PL_020", ["Type: Spot", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'SPOT', 1000, True, 0, 1.309, 0.15],
-		#["BL28_L_PL_021", ["Type: Spot", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'SPOT', 1000, True, 40000, 1.309, 0.15],
-		#["BL28_L_PL_022", ["Type: Spot", "Intensity: 1000", "Angle: 1"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 0.0174533, 0.15],
-		#["BL28_L_PL_023", ["Type: Spot", "Intensity: 1000", "Angle: 180"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 3.14159, 0.15],
-		#["BL28_L_PL_024", ["Type: Spot", "Intensity: 1000", "Falloff: 0"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 0],
-		#["BL28_L_PL_025", ["Type: Spot", "Intensity: 1000", "Falloff: 1"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 1],
+		["BL28_L_PL_008", ["Type: Sun", "Intensity: 50"], "Physical_Lights.blend", 'SUN', 50, False, 6500], 
+		["BL28_L_PL_009", ["Type: Sun", "Intensity: 1000"], "Physical_Lights.blend", 'SUN', 1000, False, 6500], 
+		["BL28_L_PL_010", ["Type: Sun", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'SUN', 0, False, 6500],
+		["BL28_L_PL_011", ["Type: Sun", "Intensity: 5000"], "Physical_Lights.blend", 'SUN', 5000, False, 6500],  
+		["BL28_L_PL_012", ["Type: Sun", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'SUN', 1000, True, 6500], 
+		["BL28_L_PL_013", ["Type: Sun", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'SUN', 1000, True, 0], 
+		["BL28_L_PL_014", ["Type: Sun", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'SUN', 1000, True, 40000], 
+		# SPOT
+		["BL28_L_PL_015", ["Type: Spot", "Intensity: 2000"], "Physical_Lights.blend", 'SPOT', 2000, False, 6500, 1.309, 0.15], 
+		["BL28_L_PL_016", ["Type: Spot", "Intensity: 1000"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 0.15],
+		["BL28_L_PL_017", ["Type: Spot", "Intensity: 0, Expected Black Picture"], "Physical_Lights.blend", 'SPOT', 0, False, 6500, 1.309, 0.15],
+		["BL28_L_PL_018", ["Type: Spot", "Intensity: 5000"], "Physical_Lights.blend", 'SPOT', 5000, False, 6500, 1.309, 0.15],
+		["BL28_L_PL_019", ["Type: Spot", "Intensity: 1000", "Temperature: 6500"], "Physical_Lights.blend", 'SPOT', 1000, True, 6500, 1.309, 0.15],
+		["BL28_L_PL_020", ["Type: Spot", "Intensity: 1000", "Temperature: 0"], "Physical_Lights.blend", 'SPOT', 1000, True, 0, 1.309, 0.15],
+		["BL28_L_PL_021", ["Type: Spot", "Intensity: 1000", "Temperature: 40 000"], "Physical_Lights.blend", 'SPOT', 1000, True, 40000, 1.309, 0.15],
+		["BL28_L_PL_022", ["Type: Spot", "Intensity: 1000", "Angle: 1"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 0.0174533, 0.15],
+		["BL28_L_PL_023", ["Type: Spot", "Intensity: 1000", "Angle: 180"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 3.14159, 0.15],
+		["BL28_L_PL_024", ["Type: Spot", "Intensity: 1000", "Falloff: 0"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 0],
+		["BL28_L_PL_025", ["Type: Spot", "Intensity: 1000", "Falloff: 1"], "Physical_Lights.blend", 'SPOT', 1000, False, 6500, 1.309, 1],
 		# AREA
 		["BL28_L_PL_026", ["Type: Area", "Intensity: 300"], "Physical_Lights.blend", 'AREA', 'RECTANGLE', 300, False, 6500, 0.1, 0.1, False, None],
 		["BL28_L_PL_027", ["Type: Area", "Intensity: 1000"], "Physical_Lights.blend", 'AREA', 'RECTANGLE', 1000, False, 6500, 0.1, 0.1, False, None],
