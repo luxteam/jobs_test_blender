582d3d3d87277a2f3fc788349a6492b06234f8bd
dm1tryG <galkin.dmitry.485@gmail.com>
2019-09-23 18:50:45 +0300

    Fix path for scene

--- jobs/Scripts/Templates/template_sun_sky.py
+++ jobs/Scripts/Templates/template_sun_sky.py
@@ -5,7 +5,7 @@ def prerender(test_list):
 
 	current_scene = bpy.path.basename(bpy.context.blend_data.filepath)
 	if current_scene != test_list[2]:
-		bpy.ops.wm.open_mainfile(filepath=os.path.join(r"{resource_path}", test_list[2]))
+		bpy.ops.wm.open_mainfile(filepath=os.path.join(r"{resource_path}, Render_Settings", test_list[2]))
 
 	scene = bpy.context.scene
 	enable_rpr_render(scene)
@@ -118,79 +118,79 @@ def prerender(test_list):
 if __name__ == "__main__":
 
 	list_tests = [
-	["BL_RS_SS_001", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 0, 0],
-	["BL_RS_SS_002", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 45", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 0, 45],
-	["BL_RS_SS_003", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 90", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 0, 90],
-	["BL_RS_SS_004", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 90, 0],
-	["BL_RS_SS_005", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 45", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 90, 45],
-	["BL_RS_SS_006", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 90", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 90, 90],
-	["BL_RS_SS_007", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 180, 0],
-	["BL_RS_SS_008", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 45", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 180, 45],
-	["BL_RS_SS_009", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 90", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 180, 90],
-	["BL_RS_SS_010", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 270, 0],
-	["BL_RS_SS_011", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 45", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 270, 45],
-	["BL_RS_SS_012", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 90", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 270, 90],
-	["BL_RS_SS_013", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 360, 0],
-	["BL_RS_SS_014", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 45", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 360, 45],
-	["BL_RS_SS_015", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 90", "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'ANALYTICAL', '1024', 360, 90],
+	["BL_RS_SS_001", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "TestSunSky.blend", 'ANALYTICAL', '1024', 0, 0],
+	["BL_RS_SS_002", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 45", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 0, 45],
+	["BL_RS_SS_003", ["Sun&Sky System: Analytical sky", "Azimuth: 0", "Altitude: 90", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 0, 90],
+	["BL_RS_SS_004", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "TestSunSky.blend", 'ANALYTICAL', '1024', 90, 0],
+	["BL_RS_SS_005", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 45", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 90, 45],
+	["BL_RS_SS_006", ["Sun&Sky System: Analytical sky", "Azimuth: 90", "Altitude: 90", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 90, 90],
+	["BL_RS_SS_007", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "TestSunSky.blend", 'ANALYTICAL', '1024', 180, 0],
+	["BL_RS_SS_008", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 45", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 180, 45],
+	["BL_RS_SS_009", ["Sun&Sky System: Analytical sky", "Azimuth: 180", "Altitude: 90", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 180, 90],
+	["BL_RS_SS_010", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "TestSunSky.blend", 'ANALYTICAL', '1024', 270, 0],
+	["BL_RS_SS_011", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 45", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 270, 45],
+	["BL_RS_SS_012", ["Sun&Sky System: Analytical sky", "Azimuth: 270", "Altitude: 90", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 270, 90],
+	["BL_RS_SS_013", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 0", "Texture Resolution: normal, Expected Black Picture"], "TestSunSky.blend", 'ANALYTICAL', '1024', 360, 0],
+	["BL_RS_SS_014", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 45", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 360, 45],
+	["BL_RS_SS_015", ["Sun&Sky System: Analytical sky", "Azimuth: 360", "Altitude: 90", "Texture Resolution: normal"], "TestSunSky.blend", 'ANALYTICAL', '1024', 360, 90],
 	
-	["BL_RS_SS_016", ["Sun&Sky System: Date, Time and Location", "Texture Resolution: small"], "Render_Settings\\TestSunSky.blend", 'textire_resolution', '256'],
-	["BL_RS_SS_017", ["Sun&Sky System: Date, Time and Location",  "Texture Resolution: normal"], "Render_Settings\\TestSunSky.blend", 'textire_resolution', '1024'],
-	["BL_RS_SS_018", ["Sun&Sky System: Date, Time and Location",  "Texture Resolution: high"], "Render_Settings\\TestSunSky.blend", 'textire_resolution', '4096'],
+	["BL_RS_SS_016", ["Sun&Sky System: Date, Time and Location", "Texture Resolution: small"], "TestSunSky.blend", 'textire_resolution', '256'],
+	["BL_RS_SS_017", ["Sun&Sky System: Date, Time and Location",  "Texture Resolution: normal"], "TestSunSky.blend", 'textire_resolution', '1024'],
+	["BL_RS_SS_018", ["Sun&Sky System: Date, Time and Location",  "Texture Resolution: high"], "TestSunSky.blend", 'textire_resolution', '4096'],
 
-	["BL_RS_SS_019", ["Sun&Sky System: Date, Time and Location", "Turbidity - 0.2"], "Render_Settings\\TestSunSky.blend", 'turbidity', '1024', 0.2],
-	["BL_RS_SS_020", ["Sun&Sky System: Date, Time and Location", "Turbidity - 1"], "Render_Settings\\TestSunSky.blend", 'turbidity', '1024', 1],
-	["BL_RS_SS_021", ["Sun&Sky System: Date, Time and Location", "Turbidity - 2"], "Render_Settings\\TestSunSky.blend", 'turbidity', '1024', 2],
-	["BL_RS_SS_022", ["Sun&Sky System: Date, Time and Location", "Turbidity - 5"], "Render_Settings\\TestSunSky.blend", 'turbidity', '1024', 5],
+	["BL_RS_SS_019", ["Sun&Sky System: Date, Time and Location", "Turbidity - 0.2"], "TestSunSky.blend", 'turbidity', '1024', 0.2],
+	["BL_RS_SS_020", ["Sun&Sky System: Date, Time and Location", "Turbidity - 1"], "TestSunSky.blend", 'turbidity', '1024', 1],
+	["BL_RS_SS_021", ["Sun&Sky System: Date, Time and Location", "Turbidity - 2"], "TestSunSky.blend", 'turbidity', '1024', 2],
+	["BL_RS_SS_022", ["Sun&Sky System: Date, Time and Location", "Turbidity - 5"], "TestSunSky.blend", 'turbidity', '1024', 5],
 	
-	["BL_RS_SS_023", ["Sun&Sky System: Date, Time and Location", "Intencity - 1"], "Render_Settings\\TestSunSky.blend", 'intencity', '1024', 1],
-	["BL_RS_SS_024", ["Sun&Sky System: Date, Time and Location", "Intencity - 2"], "Render_Settings\\TestSunSky.blend", 'intencity', '1024', 2],
-	["BL_RS_SS_025", ["Sun&Sky System: Date, Time and Location", "Intencity - 5"], "Render_Settings\\TestSunSky.blend", 'intencity', '1024', 5],
+	["BL_RS_SS_023", ["Sun&Sky System: Date, Time and Location", "Intencity - 1"], "TestSunSky.blend", 'intencity', '1024', 1],
+	["BL_RS_SS_024", ["Sun&Sky System: Date, Time and Location", "Intencity - 2"], "TestSunSky.blend", 'intencity', '1024', 2],
+	["BL_RS_SS_025", ["Sun&Sky System: Date, Time and Location", "Intencity - 5"], "TestSunSky.blend", 'intencity', '1024', 5],
 	
-	["BL_RS_SS_026", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 1"], "Render_Settings\\TestSunSky.blend", 'sun_glow', '1024', 1],
-	["BL_RS_SS_027", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 2"], "Render_Settings\\TestSunSky.blend", 'sun_glow', '1024', 2],
-	["BL_RS_SS_028", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 5"], "Render_Settings\\TestSunSky.blend", 'sun_glow', '1024', 5],
+	["BL_RS_SS_026", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 1"], "TestSunSky.blend", 'sun_glow', '1024', 1],
+	["BL_RS_SS_027", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 2"], "TestSunSky.blend", 'sun_glow', '1024', 2],
+	["BL_RS_SS_028", ["Sun&Sky System: Date, Time and Location", "Sun Glow - 5"], "TestSunSky.blend", 'sun_glow', '1024', 5],
 
-	["BL_RS_SS_029", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 0.5"], "Render_Settings\\TestSunSky.blend", 'sun_disc','1024', 0.5],
-	["BL_RS_SS_030", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 1"], "Render_Settings\\TestSunSky.blend", 'sun_disc', '1024', 1],
-	["BL_RS_SS_031", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 2"], "Render_Settings\\TestSunSky.blend", 'sun_disc', '1024', 2],
+	["BL_RS_SS_029", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 0.5"], "TestSunSky.blend", 'sun_disc','1024', 0.5],
+	["BL_RS_SS_030", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 1"], "TestSunSky.blend", 'sun_disc', '1024', 1],
+	["BL_RS_SS_031", ["Sun&Sky System: Date, Time and Location", "Sun Disc - 2"], "TestSunSky.blend", 'sun_disc', '1024', 2],
 
-	["BL_RS_SS_032", ["Sun&Sky System: Date, Time and Location", "Saturation - 0"], "Render_Settings\\TestSunSky.blend", 'saturation', '1024', 0],
-	["BL_RS_SS_033", ["Sun&Sky System: Date, Time and Location", "Saturation - 0.5"], "Render_Settings\\TestSunSky.blend", 'saturation', '1024', 0.5],
-	["BL_RS_SS_034", ["Sun&Sky System: Date, Time and Location", "Saturation - 1"], "Render_Settings\\TestSunSky.blend", 'saturation', '1024', 1],
+	["BL_RS_SS_032", ["Sun&Sky System: Date, Time and Location", "Saturation - 0"], "TestSunSky.blend", 'saturation', '1024', 0],
+	["BL_RS_SS_033", ["Sun&Sky System: Date, Time and Location", "Saturation - 0.5"], "TestSunSky.blend", 'saturation', '1024', 0.5],
+	["BL_RS_SS_034", ["Sun&Sky System: Date, Time and Location", "Saturation - 1"], "TestSunSky.blend", 'saturation', '1024', 1],
 
-	["BL_RS_SS_035", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - -0.5"], "Render_Settings\\TestSunSky.blend", 'horizon_heigh', '1024', -0.5],
-	["BL_RS_SS_036", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - 0"], "Render_Settings\\TestSunSky.blend", 'horizon_heigh', '1024', 0],
-	["BL_RS_SS_037", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - 0.5"], "Render_Settings\\TestSunSky.blend", 'horizon_heigh', '1024', 0.5],
+	["BL_RS_SS_035", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - -0.5"], "TestSunSky.blend", 'horizon_heigh', '1024', -0.5],
+	["BL_RS_SS_036", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - 0"], "TestSunSky.blend", 'horizon_heigh', '1024', 0],
+	["BL_RS_SS_037", ["Sun&Sky System: Date, Time and Location", "Horizon Heigh - 0.5"], "TestSunSky.blend", 'horizon_heigh', '1024', 0.5],
 
-	["BL_RS_SS_038", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 0.10"], "Render_Settings\\TestSunSky.blend", 'horizon_blue', '1024', 0.10],
-	["BL_RS_SS_039", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 0.50"], "Render_Settings\\TestSunSky.blend", 'horizon_blue', '1024', 0.50],
-	["BL_RS_SS_040", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 1.0"], "Render_Settings\\TestSunSky.blend", 'horizon_blue', '1024', 1.0],
+	["BL_RS_SS_038", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 0.10"], "TestSunSky.blend", 'horizon_blue', '1024', 0.10],
+	["BL_RS_SS_039", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 0.50"], "TestSunSky.blend", 'horizon_blue', '1024', 0.50],
+	["BL_RS_SS_040", ["Sun&Sky System: Date, Time and Location", "Horizon Blue - 1.0"], "TestSunSky.blend", 'horizon_blue', '1024', 1.0],
 
-	["BL_RS_SS_041", ["Sun&Sky System: Date, Time and Location", "Filter Color - 0.50"], "Render_Settings\\TestSunSky.blend", 'filter_color', '1024', (1,0,1)],
-	["BL_RS_SS_042", ["Sun&Sky System: Date, Time and Location", "Ground Color - 1.0"], "Render_Settings\\TestSunSky.blend", 'ground_color', '1024', (0,1,0)],
+	["BL_RS_SS_041", ["Sun&Sky System: Date, Time and Location", "Filter Color - 0.50"], "TestSunSky.blend", 'filter_color', '1024', (1,0,1)],
+	["BL_RS_SS_042", ["Sun&Sky System: Date, Time and Location", "Ground Color - 1.0"], "TestSunSky.blend", 'ground_color', '1024', (0,1,0)],
 	
 
-	# ["BL_RS_SS_043", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (0,0,0), "Moscow"],
-	# ["BL_RS_SS_044", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (6,0,0), "Moscow"],
-	# ["BL_RS_SS_045", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
-	# ["BL_RS_SS_046", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (18,0,0), "Moscow"],
-	# ["BL_RS_SS_047", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (24,0,0), "Moscow"],
-	# ["BL_RS_SS_048", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '256', (12,0,0), "Moscow"],
-	# ["BL_RS_SS_049", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
-	# ["BL_RS_SS_050", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '4096', (12,0,0), "Moscow"],
+	# ["BL_RS_SS_043", [""], "TestSunSky.blend", 'date_time_location', '1024', (0,0,0), "Moscow"],
+	# ["BL_RS_SS_044", [""], "TestSunSky.blend", 'date_time_location', '1024', (6,0,0), "Moscow"],
+	# ["BL_RS_SS_045", [""], "TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
+	# ["BL_RS_SS_046", [""], "TestSunSky.blend", 'date_time_location', '1024', (18,0,0), "Moscow"],
+	# ["BL_RS_SS_047", [""], "TestSunSky.blend", 'date_time_location', '1024', (24,0,0), "Moscow"],
+	# ["BL_RS_SS_048", [""], "TestSunSky.blend", 'date_time_location', '256', (12,0,0), "Moscow"],
+	# ["BL_RS_SS_049", [""], "TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
+	# ["BL_RS_SS_050", [""], "TestSunSky.blend", 'date_time_location', '4096', (12,0,0), "Moscow"],
 	
-	# ["BL_RS_SS_050", [""], "Render_Settings\\TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
+	# ["BL_RS_SS_050", [""], "TestSunSky.blend", 'date_time_location', '1024', (12,0,0), "Moscow"],
 	
-	# ["BL_RS_SS_051", [""], "Render_Settings\\TestSunSky.blend", '1', '256'],
-	# ["BL_RS_SS_052", [""], "Render_Settings\\TestSunSky.blend", '1', '1024'],
-	# ["BL_RS_SS_053", [""], "Render_Settings\\TestSunSky.blend", '1', '4096'],
-	# ["BL_RS_SS_054", [""], "Render_Settings\\TestSunSky.blend", '2', '1024', (0,0,0), "Miami_Beach"],
-	# ["BL_RS_SS_055", [""], "Render_Settings\\TestSunSky.blend", '2', '1024', (12,0,0), "Miami_Beach"],
-	# ["BL_RS_SS_056", [""], "Render_Settings\\TestSunSky.blend", '2', '1024', (24,0,0), "Miami_Beach"],
-	# ["BL_RS_SS_057", [""], "Render_Settings\\TestSunSky.blend", '3', '1024', (0,0,0), "Canberra"],
-	# ["BL_RS_SS_058", [""], "Render_Settings\\TestSunSky.blend", '3', '1024', (12,0,0), "Canberra"],
-	# ["BL_RS_SS_059", [""], "Render_Settings\\TestSunSky.blend", '3', '1024', (24,0,0), "Canberra"],
+	# ["BL_RS_SS_051", [""], "TestSunSky.blend", '1', '256'],
+	# ["BL_RS_SS_052", [""], "TestSunSky.blend", '1', '1024'],
+	# ["BL_RS_SS_053", [""], "TestSunSky.blend", '1', '4096'],
+	# ["BL_RS_SS_054", [""], "TestSunSky.blend", '2', '1024', (0,0,0), "Miami_Beach"],
+	# ["BL_RS_SS_055", [""], "TestSunSky.blend", '2', '1024', (12,0,0), "Miami_Beach"],
+	# ["BL_RS_SS_056", [""], "TestSunSky.blend", '2', '1024', (24,0,0), "Miami_Beach"],
+	# ["BL_RS_SS_057", [""], "TestSunSky.blend", '3', '1024', (0,0,0), "Canberra"],
+	# ["BL_RS_SS_058", [""], "TestSunSky.blend", '3', '1024', (12,0,0), "Canberra"],
+	# ["BL_RS_SS_059", [""], "TestSunSky.blend", '3', '1024', (24,0,0), "Canberra"],
 	]
 
 	launch_tests()
