e57d102c089911b378d5a933f39fc377e32cd6af
Andrey Kravchenko <epozine@inbox.ru>
2019-12-22 17:47:57 +0300

    Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,4 +5,4 @@ set TESTS_FILTER="%3"
 rem C:\Program Files\Blender Foundation\Blender2.8\blender.exe
 python -m pip install -r ../jobs_launcher/install/requirements.txt
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Blender28 --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender\blender.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Blender2.8Assets" PassLimit 10 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Blender28 --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender 2.81\blender.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Blender2.8Assets" PassLimit 10 rx 0 ry 0
