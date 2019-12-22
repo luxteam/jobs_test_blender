985fc1ca1b398034646ece46e1dcbdd07c7ac750
Andrei Kravchenko <epozine@inbox.ru>
2019-12-23 00:37:37 +0300

    blender 2.8 for unix

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ TESTS_FILTER="$3"
 
 python -m pip install -r ../jobs_launcher/install/requirements.txt
 
-python ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender28 --cmd_variables Tool "blender2.81" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/Blender2.8Assets" PassLimit 10 rx 0 ry 0
+python ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender28 --cmd_variables Tool "blender" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/Blender2.8Assets" PassLimit 10 rx 0 ry 0
