d2bcdf78942580106738f93bff3caa6a124015e1
kataderon <kataderon@gmail.com>
2020-03-12 18:44:08 +0300

    remove old_cases

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -202,8 +202,6 @@ if __name__ == "__main__":
 		os.makedirs(args.output)
 	except OSError as e:
 		pass
-
-	old_active_cases = 0  # number of active cases from last iteration
 	
 	try:
 		copyfile(os.path.realpath(os.path.join(os.path.dirname(
@@ -251,12 +249,10 @@ if __name__ == "__main__":
 			if case['status'] in ['active', 'fail', 'inprogress']:
 				active_cases += 1
 
-		if active_cases == 0 or old_active_cases == active_cases or iteration > len(cases):			
+		if active_cases == 0 or iteration > len(cases):			
 			# exit script if base_functions don't change number of active cases
 			kill_process(PROCESS)
 			core_config.main_logger.info(
 				"Finish simpleRender with code: {}".format(rc))
 			exit(rc)
-		
-		old_active_cases = active_cases
 
