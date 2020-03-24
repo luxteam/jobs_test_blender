bfd393562b39aae81eafe62ca8a6c74d0efe0c1c
Piromancer <Den2000_09@mail.ru>
2020-03-24 13:05:32 +0300

    Uber fix 1

--- jobs/Scripts/extensions/Uber.py
+++ jobs/Scripts/extensions/Uber.py
@@ -27,9 +27,9 @@ def create_imagemap(attr, image_name):
 	image = bpy.data.images.load(os.path.join(RES_PATH, "Maps", image_name))
 	node_imagemap.image = image
 	
-	if type(node_uber.inputs[attr]['default_value']) == tuple:
+	if type(node_uber.inputs[attr].default_value) == tuple:
 		tree.links.new(node_imagemap.outputs['Color'], node_uber.inputs[attr])
-	elif type(node_uber.inputs[attr]['default_value']) == float:
+	elif type(node_uber.inputs[attr].default_value) == float:
 		tree.links.new(node_imagemap.outputs['Alpha'], node_uber.inputs[attr])
 
 
@@ -53,65 +53,65 @@ def default_settings():
 	set_value(node_uber, "enable_diffuse", True)
 	set_value(node_uber, "diffuse_use_shader_normal", True)
 	set_value(node_uber, "separate_backscatter_color", False)
-	node_uber.inputs['Diffuse Color']['default_value'] = (0.033, 0.26, 0.5, 1.0)
-	node_uber.inputs['Diffuse Weight']['default_value'] = 1.0
-	node_uber.inputs['Diffuse Roughness']['default_value'] = 0.5
-	node_uber.inputs['Backscatter Color']['default_value'] = (0.5, 0.5, 0.5, 1.0)
-	node_uber.inputs['Backscatter Weight']['default_value'] = 0
+	node_uber.inputs['Diffuse Color'].default_value = (0.033, 0.26, 0.5, 1.0)
+	node_uber.inputs['Diffuse Weight'].default_value = 1.0
+	node_uber.inputs['Diffuse Roughness'].default_value = 0.5
+	node_uber.inputs['Backscatter Color'].default_value = (0.5, 0.5, 0.5, 1.0)
+	node_uber.inputs['Backscatter Weight'].default_value = 0
 
 	set_value(node_uber, "enable_reflection", True)
 	set_value(node_uber, "reflection_mode", 'PBR')
 	set_value(node_uber, "reflection_use_shader_normal", True)
-	node_uber.inputs['Reflection Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
-	node_uber.inputs['Reflection IOR']['default_value'] = 2.4
-	node_uber.inputs['Reflection Anisotropy']['default_value'] = 0.0
-	node_uber.inputs['Reflection Anisotropy Rotation']['default_value'] = 0.0
-	node_uber.inputs['Reflection Metalness']['default_value'] = 0.0
-	node_uber.inputs['Reflection Roughness']['default_value'] = 0.4
-	node_uber.inputs['Reflection Weight']['default_value'] = 1.0
+	node_uber.inputs['Reflection Color'].default_value = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Reflection IOR'].default_value = 2.4
+	node_uber.inputs['Reflection Anisotropy'].default_value = 0.0
+	node_uber.inputs['Reflection Anisotropy Rotation'].default_value = 0.0
+	node_uber.inputs['Reflection Metalness'].default_value = 0.0
+	node_uber.inputs['Reflection Roughness'].default_value = 0.4
+	node_uber.inputs['Reflection Weight'].default_value = 1.0
 	
 	set_value(node_uber, "enable_coating", False)
 	set_value(node_uber, "coating_use_shader_normal", True)
-	node_uber.inputs['Coating Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
-	node_uber.inputs['Coating Weight']['default_value'] = 1.0
-	node_uber.inputs['Coating IOR']['default_value'] = 1.5
-	node_uber.inputs['Coating Roughness']['default_value'] = 0.01
-	node_uber.inputs['Coating Thickness']['default_value'] = 0
-	node_uber.inputs['Coating Transmission Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Coating Color'].default_value = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Coating Weight'].default_value = 1.0
+	node_uber.inputs['Coating IOR'].default_value = 1.5
+	node_uber.inputs['Coating Roughness'].default_value = 0.01
+	node_uber.inputs['Coating Thickness'].default_value = 0
+	node_uber.inputs['Coating Transmission Color'].default_value = (1.0, 1.0, 1.0, 1.0)
    
 	set_value(node_uber, "enable_refraction", False)
 	set_value(node_uber, "refraction_caustics", False)
 	set_value(node_uber, "refraction_thin_surface", False)
 	set_value(node_uber, "refraction_use_shader_normal", True)
-	node_uber.inputs['Refraction Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
-	node_uber.inputs['Refraction Weight']['default_value'] = 1.0
-	node_uber.inputs['Refraction Roughness']['default_value'] = 0
-	node_uber.inputs['Refraction IOR']['default_value'] = 1.5
-	node_uber.inputs['Refraction Absorption Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
-	node_uber.inputs['Refraction Absorption Distance']['default_value'] = 0
+	node_uber.inputs['Refraction Color'].default_value = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Refraction Weight'].default_value = 1.0
+	node_uber.inputs['Refraction Roughness'].default_value = 0
+	node_uber.inputs['Refraction IOR'].default_value = 1.5
+	node_uber.inputs['Refraction Absorption Color'].default_value = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Refraction Absorption Distance'].default_value = 0
 					  
 	set_value(node_uber, "enable_sheen", False)
-	node_uber.inputs['Sheen Color']['default_value'] = (0.5, 0.5, 0.5, 1.0)
-	node_uber.inputs['Sheen Tint']['default_value'] = 0.5
-	node_uber.inputs['Sheen Weight']['default_value'] = 1.0
+	node_uber.inputs['Sheen Color'].default_value = (0.5, 0.5, 0.5, 1.0)
+	node_uber.inputs['Sheen Tint'].default_value = 0.5
+	node_uber.inputs['Sheen Weight'].default_value = 1.0
 	
 	set_value(node_uber, "enable_emission", False)
 	set_value(node_uber, "emission_doublesided", False)
 	set_value(node_uber, "emission_intensity", 1.0)
-	node_uber.inputs['Emission Color']['default_value'] = (1.0, 1.0, 1.0, 1.0)
-	node_uber.inputs['Emission Weight']['default_value'] = 1.0
+	node_uber.inputs['Emission Color'].default_value = (1.0, 1.0, 1.0, 1.0)
+	node_uber.inputs['Emission Weight'].default_value = 1.0
 	
 	set_value(node_uber, "enable_sss", False)
 	set_value(node_uber, "sss_multiscatter", False)
 	set_value(node_uber, "sss_use_diffuse_color", False)
-	node_uber.inputs['Subsurface Color']['default_value'] = (0.436, 0.227, 0.131, 1.0)
-	node_uber.inputs['Subsurface Direction']['default_value'] = 0
-	node_uber.inputs['Subsurface Radius']['default_value'] = (3.67, 1.37, 0.68)
-	node_uber.inputs['Subsurface Weight']['default_value'] = 1.0
+	node_uber.inputs['Subsurface Color'].default_value = (0.436, 0.227, 0.131, 1.0)
+	node_uber.inputs['Subsurface Direction'].default_value = 0
+	node_uber.inputs['Subsurface Radius'].default_value = (3.67, 1.37, 0.68)
+	node_uber.inputs['Subsurface Weight'].default_value = 1.0
 
 	set_value(node_uber, "enable_normal", False)
 
 	set_value(node_uber, "enable_transparency", False)
-	node_uber.inputs['Transparency']['default_value'] = 0
+	node_uber.inputs['Transparency'].default_value = 0
 
 	set_value(node_uber, "enable_displacement", False)
