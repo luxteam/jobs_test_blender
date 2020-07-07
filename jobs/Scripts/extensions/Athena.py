f4d6fc6959a3f1ec1e55e2e9af5885a1de20df7a
Piromancer <Den2000_09@mail.ru>
2020-07-07 16:41:58 +0300

    4 more athena cases

--- jobs/Scripts/extensions/Athena.py
+++ jobs/Scripts/extensions/Athena.py
@@ -1,5 +1,8 @@
+CONFIG_PATH = 'C:/Users/user/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py'
+ATHENA_DIR = 'C:/Users/user/AppData/Local/Temp/rprblender/'
+
 def set_clean_false():
-    with open('C:/Users/user/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py', 'r') as file:
+    with open(CONFIG_PATH, 'r') as file:
         data = file.readlines()
     i = 0
     for s in data:
@@ -8,11 +11,11 @@ def set_clean_false():
             break
         i += 1
 
-    with open('C:/Users/user/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py', 'w') as file:
+    with open(CONFIG_PATH, 'w') as file:
         file.writelines(data)
 
 def set_clean_true():
-    with open('C:/Users/user/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py', 'r') as file:
+    with open(CONFIG_PATH, 'r') as file:
         data = file.readlines()
     i = 0
     for s in data:
@@ -21,21 +24,21 @@ def set_clean_true():
             break
         i += 1
 
-    with open('C:/Users/user/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py', 'w') as file:
+    with open(CONFIG_PATH, 'w') as file:
         file.writelines(data)
 
-def validate_athena():
-    athena_files = os.listdir("C:/Users/user/AppData/Local/Temp/rprblender")
+def validate_athena(case):
+    athena_files = os.listdir(ATHENA_DIR)
     if(not athena_files):
-        copyfile(path.join('..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'error.jpg'), path.join(WORK_DIR, 'Color', 'BL28_RS_ATH_001.jpg'))
+        copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'error.jpg'), path.join(WORK_DIR, 'Color', case['case'] + ".jpg"))
     # this is separate from try except, because athena_file.read() causes exception
-    with open("C:/Users/user/AppData/Local/Temp/rprblender/" + athena_files[0], "r") as athena_file:
+    with open(ATHENA_DIR + athena_files[0], "r") as athena_file:
         print("Athena file content:")
         print(athena_file.read())
     try:
-        with open("C:/Users/user/AppData/Local/Temp/rprblender/" + athena_files[0], "r") as athena_file:
+        with open(ATHENA_DIR + athena_files[0], "r") as athena_file:
             data = json.load(athena_file)
-            copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'passed.jpg'), path.join(WORK_DIR, 'Color', 'BL28_RS_ATH_001.jpg'))
+            copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'passed.jpg'), path.join(WORK_DIR, 'Color', case['case'] + ".jpg"))
     except Exception as e:
         print(e)
-        copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'error.jpg'), path.join(WORK_DIR, 'Color', 'BL28_RS_ATH_001.jpg'))
+        copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'error.jpg'), path.join(WORK_DIR, 'Color', case['case'] + ".jpg"))
