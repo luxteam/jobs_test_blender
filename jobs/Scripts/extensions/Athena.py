b20ebc048b201b971f4db3c5a6701dd0d640f637
Piromancer <Den2000_09@mail.ru>
2020-07-10 13:30:57 +0300

    Removed comments

--- jobs/Scripts/extensions/Athena.py
+++ jobs/Scripts/extensions/Athena.py
@@ -3,22 +3,11 @@ import getpass
 
 if (platform.system() == 'Windows'):
     CONFIG_PATH = os.path.expandvars('%appdata%/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py')
-    #CONFIG_PATH = 'C:/Users/' + getpass.getuser() + '/AppData/Roaming/Blender Foundation/Blender/2.83/scripts/addons/rprblender/config.py'
-    ATHENA_DIR = 'C:/Users/' + getpass.getuser() + '/AppData/Local/Temp/rprblender/'
+    ATHENA_DIR = os.path.expandvars('%appdata%/../Local/Temp/rprblender/')
 elif (platform.system() == 'Darwin'):
     CONFIG_PATH = '/Users/' + getpass.getuser() + '/Library/Application Support/Blender/2.83/scripts/addons/rprblender/config.py'
     ATHENA_DIR = os.environ['TMPDIR'] + 'rprblender/'
-# else:
-#     didn't implement Ubuntu
-#     CONFIG_PATH = ''
-#     ATHENA_DIR = os.environ['TMPDIR'] + 'rprblender/'
-
-# def set_clean(value):
-#     with open(CONFIG_PATH, 'r') as file:
-#         config = file.read()
-#     config.replace('clean_athena_files = ' + str(not value), 'clean_athena_files = ' + str(value))
-#     with open(CONFIG_PATH, 'w') as file:
-#         file.write(config)
+# TODO implement Ubuntu
 
 def validate_athena(case):
     athena_files = os.listdir(ATHENA_DIR)
@@ -27,7 +16,7 @@ def validate_athena(case):
         copyfile(path.join(WORK_DIR, '..', '..', '..', '..', 'jobs_launcher', 'common', 'img', 'error.jpg'), path.join(WORK_DIR, 'Color', case['case'] + '.jpg'))
     json_file = athena_files[0]
     try:
-        with open(ATHENA_DIR + json_file, 'r') as athena_file:
+        with open(path.join(ATHENA_DIR, json_file), 'r') as athena_file:
             logging('Athena file content:\n' + athena_file.read())
             athena_file.seek(0)
             data = json.load(athena_file)
