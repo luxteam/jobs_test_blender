b6d977121f37136755522564425c4986070f08db
Piromancer <Den2000_09@mail.ru>
2020-09-08 11:11:57 +0300

    Trying to fix github

--- jobs/Scripts/extensions/CPU_Mode.py
+++
@@ -1,4 +0,0 @@
-def enable_cpu_mode():
-    render_device_settings = get_user_settings().final_devices
-    set_value(render_device_settings, 'cpu_state', True)
-    render_device_settings.gpu_states[0] = False
