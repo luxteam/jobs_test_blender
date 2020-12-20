8f409360ab33a576b7080a3dacb91254e49fdb6b
Flame0n <36414392+Flame0n@users.noreply.github.com>
2020-12-21 00:14:30 +0300

    Replace requirements.txt to script folder

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,4 +1,4 @@
-set PATH=c:\python35\;c:\python35\scripts\;%PATH%
+set PATH=c:\python39\;c:\python39\scripts\;%PATH%
 set RENDER_DEVICE=%1
 set FILE_FILTER=%2
 set TESTS_FILTER="%3"
@@ -25,6 +25,8 @@ if not defined TOOL set TOOL=2.90
 if not defined RETRIES set RETRIES=2
 if not defined UPDATE_REFS set UPDATE_REFS="No"
 
-python -m pip install -r ../jobs_launcher/install/requirements.txt
+python -m pip install -r requirements.txt
 
 python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Blender28 --cmd_variables Tool "C:\Program Files\Blender Foundation\Blender %TOOL%\blender.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Blender2.8Assets" PassLimit %ITER% rx %RX% ry %RY% SPU %SPU% threshold %THRESHOLD% engine %ENGINE% retries %RETRIES% UpdateRefs %UPDATE_REFS%
+
+TIMEOUT /t 1000
